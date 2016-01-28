ea57a144f45f53973a9e1aad04c87ac83885adb8
evelynzhang88 <1054982653@qq.com>
2016-01-28 17:22:21 +0800

    init the project

--- js/j-mobile.js
+++ js/j-mobile.js
@@ -1,3 +1,25 @@
 /**
  * Created by james on 16/1/26.
  */
+//wayppoint
+$(document).ready(function(){
+    window.onload = function(){
+        var arr = [];
+        for(var i=0;i<$('.tg__wrap').length;i++){
+            arr.push($('.tg__wrap')[i].offsetTop);
+        }
+        console.log(arr)
+        $(window).on('scroll', function(e){
+
+            for(var j=0;j<arr.length;j++){
+                if($(window).scrollTop()>arr[j]){
+                    $('.tg__wrap').removeClass('actived').eq(j).addClass('actived');
+                }
+            }
+
+        });
+    };
+
+});
+
+
